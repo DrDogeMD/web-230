0d19024594c14e75c22d769a0d43cb981e5006ee
DrDogeMD <tmalcala090@gmail.com>
2019-06-05 21:37:53 -0500

    Commit for assignment 1.3

---
+++ week-1/hello-world.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date: 4 June 2019
+; Modified By: Troy Alcala
+; Description: This program outputs a basic
+; "hello world" script.
+;===========================================
+*/ 
+
+console.log("Hello World, my name is Troy Alcala"); /*outputs text between quotation marks*/
