8930792958a345e91820f4ad34e9260884f23c69
DrDogeMD <tmalcala090@gmail.com>
2019-06-05 21:56:51 -0500

    Commit for assignment 1.4

---
+++ week-1/alcala-primitive-types.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Troy Alcala
+; Date: 4 June 2019
+; Description: This program demonstrates 
+; primitive type variables and how to
+; output them.
+;===========================================
+*/ 
+
+var firstName = "Troy";
+var lastName = " Alcala";
+var age = " 25";             /*create variables, while adding spaces for readability*/
+var street = " 1000 Galvin Rd.";
+var city = " Bellevue,";     /*adding comma for readability*/
+var zipCode = " 68005";
+
+console.log(firstName + lastName + age + street + city + zipCode) /*outputting information from each variable*/
